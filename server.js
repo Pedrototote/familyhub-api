d9b542b0b44c1bb3f5b54b3eccbc168a08f968b5
Pedro Beas <pedro.beas@gmail.com>
2026-08-24 22:48:19 -0400

    Add environment variables for Railway

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const SECRET_KEY = 'tu_clave_super_secreta_familyhub_2024';
 
 // Pool de conexiones a MySQL
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'textoper_fhuser',
-  password: '-SHAroY1R&T+$A34',
-  database: 'tuusuario_familyhub',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
